ea6951cf7d74260751ace47f704509d1a9edabda
Ian Wambua <ianmasai21@gmail.com>
2022-09-12 05:12:31 -0700

    fiuer

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main -entry point
+ * Description: printing alphabets
+ * followed by new line
+ * here lies the description for this file
+ * Return:always 0 (success)
+ */
 int main(void)
 {
 	int ch;
