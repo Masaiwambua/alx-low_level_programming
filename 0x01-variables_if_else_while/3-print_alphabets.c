6c769cb13131b9637fee05d064f324896fdc3608
Ian Wambua <ianmasai21@gmail.com>
2022-09-12 04:55:07 -0700

    jhjfh

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,19 +2,16 @@
 
 /**
  * main -entry
- * here lies the description for this file 
+ * here lies the description for this file
+ * retun always 0 (success)
  */
-
 int main(void)
 {
-	
 	int ch;
-
 	for (ch = 97; ch <= 122; ch++)
 		putchar(ch);
 	for (ch = 65; ch <= 90; ch++)
 		putchar(ch);
 	putchar(10);
-
 	return (0);
 }
