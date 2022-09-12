db6dd7f67efbec8c01805aec1df180b870d84218
Ian Wambua <ianmasai21@gmail.com>
2022-09-12 04:41:08 -0700

    gosh

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-int main ()
+int main(void)
 {
 	int ch;
 
-	for (ch=97; ch<=122; ch++){
-		if ((ch==113) || (ch==101))
+	for (ch = 97; ch <= 122; ch++)
+	{
+		if ((ch == 113) || (ch == 101))
 			continue;
 		putchar(ch);
 	}
 
 	putchar(10);
 
-	return 0;
+	return (0);
 }
