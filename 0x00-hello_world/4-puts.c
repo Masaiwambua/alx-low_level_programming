ab64c5944c79bee41efd64c0a5c3395761d7f88d
Ian Wambua <ianmasai21@gmail.com>
2022-09-12 06:08:40 -0700

    some last minute changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-int main()
+/**
+ * main -entry point
+ * Description: printing alphabets
+ * followed by new line
+ * here lies the description for this file
+ * Return:always 0 (success)
+ */
+int main(void)
 {
 	puts("programming is like building a multilingual puzzle\n");
 	
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main -entry point
+ * Description: printing alphabets
+ * followed by new line
+ * here lies the description for this file
+ * Return:always 0 (success)
+ */
 
-int main ()
+int main (void)
 {
 	printf("Size of char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
@@ -8,5 +15,5 @@ int main ()
 	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-	return 0;
+	return (0);
 }
