fcf56c29cab1d8869f940ac75a36a6480d2b45e1
Ian Wambua <ianmasai21@gmail.com>
2022-09-14 06:05:19 -0700

    news

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: always 0.
+ * 
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{{
+		int ch;
+		for (ch = 97; ch <= 122; ch++)
+			putchar (ch);}
+	i++;
+	putchar (10);
+
+	}
+	 return (0);
+}
