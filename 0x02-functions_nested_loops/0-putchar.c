b79e05af596f7902fe0d3b1c2c1884499e523881
Ian Wambua <ianmasai21@gmail.com>
2022-09-22 06:16:24 -0700

    new strides

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,7 +10,8 @@ int main(void)
 {
 	int ch[] = {(95), (112), (117), (116), (99), (104), (97), (114)};
 
-	printf("%c%c%c%c%c%c%c%c", ch[0], ch[1], ch[2], ch[3], ch[4], ch[5], ch[6], ch[7]);
+	printf("%c%c%c%c%c%c%c", ch[0], ch[1], ch[2], ch[3], ch[4], ch[5], ch[6]);
+	printf("%c", ch[7]);
 
 	putchar(10);
 
