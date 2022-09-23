fbf5fd6b95e94deb29b50a029e252cd37f8f6546
Ian Wambua <ianmasai21@gmail.com>
2022-09-23 08:16:25 -0700

    new beginnings

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+
+
+these files are my fouth attempt at cracking c
+I'll add more details come November
+
+
---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,20 @@
+
+#include "main.h"
+
+/**
+ *int main - calls other functions
+ *
+ *Return : Always 0
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 88;
+
+	printf("%c: %d\n", c, _isupper(c));
+
+	return (0);
+
+}
